884f2ed95bd950db12ff884c5e4e461e322ebc49
Jefferson Oliveira <cardbeatt@gmail.com>
2019-01-24 20:18:35 -0200

    added components and altered app.js

--- App.js
+++ App.js
@@ -1,13 +1,35 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { createStackNavigator } from 'react-navigation';
+import BoardScreen from './components/BoardScreen';
+import BoardDetailScreen from './components/BoardDetailScreen';
+import AddBoardScreen from './components/AddBoardScreen';
+import EditBoardScreen from './components/EditBoardScreen';
+
+const RootStack = createStackNavigator(
+  {
+    Board: BoardScreen,
+    BoardDetails: BoardDetailScreen,
+    AddBoard: AddBoardScreen,
+    EditBoard: EditBoardScreen,
+  },
+  {
+    initialRouteName: 'Board',
+    navigationOptions: {
+      headerStyle: {
+        backgroundColor: '#777777',
+      },
+      headerTintColor: '#fff',
+      headerTitleStyle: {
+        fontWeight: 'bold',
+      },
+    },
+  },
+);
 
 export default class App extends React.Component {
   render() {
-    return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-      </View>
-    );
+    return <RootStack />;
   }
 }
 
