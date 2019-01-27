abb1ff5f31ca0a5af612e2d08b41d71d8d10f996
Jefferson Oliveira <cardbeatt@gmail.com>
2019-01-27 18:00:56 -0200

    switchnavigator

--- App.js
+++ App.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+// import the different screens
+import { SwitchNavigator } from 'react-navigation'
+import Loading from './components/Loading'
+import SignUp from './components/SignUp'
+import Login from './components/Login'
+import Main from './components/Main'
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+        < switcher />
       </View>
     );
   }
@@ -19,3 +25,16 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+// create our app's navigation stack
+const switcher = SwitchNavigator(
+  {
+    Loading,
+    SignUp,
+    Login,
+    Main
+  },
+  {
+    initialRouteName: 'Loading'
+  }
+)
