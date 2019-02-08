e24572f8671d3deb4eecec5f940f97fe98958710
Jefferson Oliveira <cardbeatt@gmail.com>
2019-02-08 21:05:54 -0200

    added firestore to get posts

--- components/Dashboard.js
+++ components/Dashboard.js
@@ -1,9 +1,30 @@
 import React, {Component} from 'react'
 import { StyleSheet, Text, View} from 'react-native'
-
+import firebase from '../Firebase'
 
 
 export default class Dashboard extends Component {
+    constructor(props) {
+        super()
+        this.state = {
+            data: []
+        }
+    }
+
+    componentWillMount() {
+        const db = firebase.firestore();
+        const posts = db.collection('posts')
+        posts.get().then( snapshot => {
+            snapshot.forEach( doc => {
+                this.setState({
+                    data: [...this.state.data, {id: doc.id, data: doc.data() }]
+                })
+            })
+        })
+    }
+
+
+
     render() {
         return (
             <View style={styles.dashboard}>
