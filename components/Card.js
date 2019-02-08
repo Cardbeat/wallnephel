e635a8b4682b14474a2040e6bf2784f0573c769b
Jefferson Oliveira <cardbeatt@gmail.com>
2019-02-08 21:20:22 -0200

    storage

--- components/Card.js
+++ components/Card.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import { View, Text} from 'react-native'
 
 export default class Card extends Component {
+    // fazer baixar a imagem pelo nome do storage
     render() {
         return (
             <Text>
